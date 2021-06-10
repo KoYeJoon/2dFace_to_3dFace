955ce0652c90f0d845b6b066e228bfdb649242d5
yejoon <yejoon08@ajou.ac.kr>
2021-06-10 13:14:22 +0900

    modify qt.py because of bugs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
-import os
+
 sys.path.append("./SR/src")
 sys.path.append("./fuse_deep3d")
-# sys.path.append("./fuse_deep3d/src_3d")
-from fuse_deep3d import reconstruction3d
+
 from SR.src import test_enhance_single_unalign
 from fuse_deep3d import reconstruction3d
 from SR.src.options.test_options import TestOptions
